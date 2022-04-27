92087cbb5cf609180ad4f3c8d4695f61123313a9
Zdravkoski <lazar@samplesolutions.eu>
2022-04-27 22:05:52 +0200

    test connection

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ def get_weather():
             password="lazar",
             host="127.0.0.1",
             port=3306,
-            database="employees"
+            database="capston_project"
 
     )
     except mariadb.Error as e:
