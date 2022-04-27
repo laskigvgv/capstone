86a5407a27a07747d3ea3142616cf6b07ff281e0
Zdravkoski <lazar@samplesolutions.eu>
2022-04-27 12:33:19 +0200

    no while loop

--- config.py
+++ config.py
@@ -9,33 +9,33 @@ def get_weather():
     sense.clear()
     sense.rotation = 90
     try:
-        while True:
-            temp = sense.get_temperature()
-            temp = round(temp, 1)
-            print("Temperature C", temp) 
+       
+        temp = sense.get_temperature()
+        temp = round(temp, 1)
+        print("Temperature C", temp) 
 
 
-            humidity = sense.get_humidity()  
-            humidity = round(humidity, 1)  
-            print("Humidity :",humidity)  
+        humidity = sense.get_humidity()  
+        humidity = round(humidity, 1)  
+        print("Humidity :",humidity)  
 
-            pressure = sense.get_pressure()
-            pressure = round(pressure, 1)
-            print("Pressure:",pressure)
+        pressure = sense.get_pressure()
+        pressure = round(pressure, 1)
+        print("Pressure:",pressure)
 
-            localtime = time.asctime( time.localtime(time.time()) )
-            print ("Local current time :", localtime)
-            
-            red = (255 , 0 , 0)
-            green = (0 , 255 , 0)
-            blue = (0 , 0 , 255)
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ("Local current time :", localtime)
+        
+        red = (255 , 0 , 0)
+        green = (0 , 255 , 0)
+        blue = (0 , 0 , 255)
 
-            sense.show_message(str(temp) +" C",0.2,text_colour=red)
-            sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
-            sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
+        sense.show_message(str(temp) +" C",0.2,text_colour=red)
+        sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
+        sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
 
-            sense.clear()
-            time.sleep(30)
+        sense.clear()
+        
 
     except Exception as error_msg:
         sense.clear()
