388e24a1a1a207a7ad8446e243e84dd1b5c09179
Zdravkoski <lazar@samplesolutions.eu>
2022-04-27 22:08:29 +0200

    test connection

--- config.py
+++ config.py
@@ -33,40 +33,40 @@ def get_weather():
     cur = conn.cursor()
 
 
-    # try:
+    try:
        
-    #     temp = sense.get_temperature()
-    #     temp = round(temp, 1)
-    #     # engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
-    #     print("Temperature C", temp) 
+        temp = sense.get_temperature()
+        temp = round(temp, 1)
+        # engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
+        print("Temperature C", temp) 
 
 
-    #     humidity = sense.get_humidity()  
-    #     humidity = round(humidity, 1)  
-    #     print("Humidity :",humidity)  
+        humidity = sense.get_humidity()  
+        humidity = round(humidity, 1)  
+        print("Humidity :",humidity)  
 
-    #     pressure = sense.get_pressure()
-    #     pressure = round(pressure, 1)
-    #     print("Pressure:",pressure)
+        pressure = sense.get_pressure()
+        pressure = round(pressure, 1)
+        print("Pressure:",pressure)
 
-    #     localtime = time.asctime( time.localtime(time.time()) )
-    #     print ("Local current time :", localtime)
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ("Local current time :", localtime)
 
-    #     engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
-    #     print("zapisana data")
-    #     red = (255 , 0 , 0)
-    #     green = (0 , 255 , 0)
-    #     blue = (0 , 0 , 255)
+        cur.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
+        print("zapisana data")
+        red = (255 , 0 , 0)
+        green = (0 , 255 , 0)
+        blue = (0 , 0 , 255)
 
-    #     sense.show_message(str(temp) +" C",0.2,text_colour=red)
-    #     sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
-    #     sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
+        sense.show_message(str(temp) +" C",0.2,text_colour=red)
+        sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
+        sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
 
-    #     sense.clear()
+        sense.clear()
         
 
-    # except Exception as error_msg:
-    #     sense.clear()
-    #     return {'error':error_msg}
+    except Exception as error_msg:
+        sense.clear()
+        return {'error':error_msg}
 
 
