711627d01438c022ea1e8e1377eb7d33ce2acfce
Zdravkoski <lazar@samplesolutions.eu>
2022-04-27 21:34:57 +0200

    local database

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ def get_weather():
 
     try:
         print("pred zemanje konekcija")
-        engine = sql.create_engine("mysql+pymysql://{username}:{passw}@localhost:3306/".format(username = "root", passw ="L.z3008994473001"))
+        engine = sql.create_engine("mysql+pymysql://{username}:{passw}@localhost:3306/".format(username = "capstone", passw ="lazar"))
         engine.connect()
-        print("zemena konekcija"))
+        print("zemena konekcija")
     except:
         return {
 				"error": {
@@ -28,40 +28,40 @@ def get_weather():
 			}
 
 
-    # try:
+    try:
        
-    #     temp = sense.get_temperature()
-    #     temp = round(temp, 1)
-    #     # engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
-    #     print("Temperature C", temp) 
+        temp = sense.get_temperature()
+        temp = round(temp, 1)
+        # engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
+        print("Temperature C", temp) 
 
 
-    #     humidity = sense.get_humidity()  
-    #     humidity = round(humidity, 1)  
-    #     print("Humidity :",humidity)  
+        humidity = sense.get_humidity()  
+        humidity = round(humidity, 1)  
+        print("Humidity :",humidity)  
 
-    #     pressure = sense.get_pressure()
-    #     pressure = round(pressure, 1)
-    #     print("Pressure:",pressure)
+        pressure = sense.get_pressure()
+        pressure = round(pressure, 1)
+        print("Pressure:",pressure)
 
-    #     localtime = time.asctime( time.localtime(time.time()) )
-    #     print ("Local current time :", localtime)
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ("Local current time :", localtime)
 
-    #     engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
-    #     print("zapisana data")
-    #     red = (255 , 0 , 0)
-    #     green = (0 , 255 , 0)
-    #     blue = (0 , 0 , 255)
+        engine.execute('INSERT INTO `weather_data`.`weather_station_data`(`temperature`,`humidity`,`weather_station_datacol`)VALUES({},{},{});'.format(temp,humidity,pressure))
+        print("zapisana data")
+        red = (255 , 0 , 0)
+        green = (0 , 255 , 0)
+        blue = (0 , 0 , 255)
 
-    #     sense.show_message(str(temp) +" C",0.2,text_colour=red)
-    #     sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
-    #     sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
+        sense.show_message(str(temp) +" C",0.2,text_colour=red)
+        sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
+        sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
 
-    #     sense.clear()
+        sense.clear()
         
 
-    # except Exception as error_msg:
-    #     sense.clear()
-    #     return {'error':error_msg}
+    except Exception as error_msg:
+        sense.clear()
+        return {'error':error_msg}
 
 
