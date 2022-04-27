57c33d213ed487fdce0e52cf69948bbe03706e11
Zdravkoski <lazar@samplesolutions.eu>
2022-04-27 11:55:12 +0200

    test2

--- config.py
+++ config.py
@@ -22,18 +22,18 @@ try:
         pressure = round(pressure, 1)
         print("Pressure:",pressure)
 
-    localtime = time.asctime( time.localtime(time.time()) )
-    print ("Local current time :", localtime)
+        localtime = time.asctime( time.localtime(time.time()) )
+        print ("Local current time :", localtime)
 
-    red = (255 , 0 , 0)
-    green = (0 , 255 , 0)
-    blue = (0 , 0 , 255)
+        red = (255 , 0 , 0)
+        green = (0 , 255 , 0)
+        blue = (0 , 0 , 255)
 
-    #sense.show_message(str(temp) +" C",0.2,text_colour=red)
-    #sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
-    #sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
+        #sense.show_message(str(temp) +" C",0.2,text_colour=red)
+        #sense.show_message(str(humidity) + "%" ,0.2,text_colour=green)
+        #sense.show_message(str(pressure) +"hPa",0.2,text_colour=blue)
 
-    time.sleep(1)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     pass
