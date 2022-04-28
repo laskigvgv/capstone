19a4ca3d7b128fe01b6c7156eed7d0f28b4771cf
Zdravkoski <lazar@samplesolutions.eu>
2022-04-28 17:29:04 +0200

    echo one result

--- weather/index.php
+++ weather/index.php
@@ -83,7 +83,7 @@
 									$sql = "SELECT DATE(time_stamp) FROM weather_data;";
 									$result = $conn->query($sql)->fetch_assoc();
 
-									var_dump($result);
+									var_dump($result[0]);
 
 								?>
 							</div>
