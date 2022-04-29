5cd0c0ff5c421a0a4c91a00ea1110cd868a067b8
Zdravkoski <lazar@samplesolutions.eu>
2022-04-29 20:43:19 +0200

    test gif

--- weather/index.php
+++ weather/index.php
@@ -143,7 +143,7 @@
 						<div class="row">
 							<div class="col-md-3 col-sm-6">
 								<div class="live-camera">
-									<figure class="live-camera-cover"><img src="images/gostivar_night1.jpg" alt=""></figure>
+									<div class="live-camera-cover"><img src="images/gostivar_night1.jpg" alt=""></div>
 									<h3 class="location">New York</h3>
 									<small class="date">8 oct, 8:00AM</small>
 								</div>
