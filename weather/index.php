c08c03764ae80cc613b1185de478c555db6e894b
Zdravkoski <lazar@samplesolutions.eu>
2022-04-29 16:50:08 +0200

    test gif

--- weather/index.php
+++ weather/index.php
@@ -99,7 +99,7 @@
 											echo $result["tmp"]
 										?>
 										
-										<sup>o</sup>C</div><img src="images/icons/temperature.gif" width="45px" height="45px" loop=infinite>
+										<sup>o</sup>C</div><img src="images/icons/temperature.gif" width="45px" height="45px" loop="infinite">
 								</div>
 							</div>
 						</div>
