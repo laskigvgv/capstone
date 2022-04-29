1da549f16699361a5b07123917d8cd771ed23701
Zdravkoski <lazar@samplesolutions.eu>
2022-04-29 20:39:17 +0200

    test gif

--- weather/index.php
+++ weather/index.php
@@ -139,7 +139,7 @@
 			<main class="main-content">
 				<div class="fullwidth-block">
 					<div class="container">
-						<h2 class="section-title">Live cameras</h2>
+						<h2 class="section-title">Gostivar In Winter</h2>
 						<div class="row">
 							<div class="col-md-3 col-sm-6">
 								<div class="live-camera">
