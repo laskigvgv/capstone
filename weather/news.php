57cf1ccfd066c47c0c3a12de0e76795c702d2395
Zdravkoski <lazar@samplesolutions.eu>
2022-08-24 22:02:16 +0200

    menu links

--- weather/news.php
+++ weather/news.php
@@ -95,7 +95,7 @@
 								?>
 							</div>
 								<div class="date">Last Measurement Time:	<?php 
-									echo json_encode($result); ?>
+									echo $result; ?>
 								</div>
 							</div> <!-- .forecast-header -->
 							<div class="forecast-content">
