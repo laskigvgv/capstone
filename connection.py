99980fbd79ad38cf19a230cc863029ea672c3c43
Zdravkoski <lazar@samplesolutions.eu>
2022-08-24 18:19:07 +0200

    7 days forecast

--- connection.py
+++ connection.py
@@ -48,10 +48,10 @@ for i in range(1,8):
         "humidity": humidity
     }
 
-    querr = """INSERT INTO `week_forecast` (`one_week`)VALUES(%s);"""
-    data = (json.dumps(dict_from_api))
+querr = """INSERT INTO `week_forecast` (`one_week`)VALUES(%s);"""
+data = (json.dumps(dict_from_api))
 
-    pprint("USPESNO VNESEN ZAPIS")
+pprint("USPESNO VNESEN ZAPIS")
 
     
 
