ee8b6295017c7781ac684c5e270d30d6ca2501d3
Zdravkoski <lazar@samplesolutions.eu>
2022-08-24 22:30:29 +0200

    menu links

--- weather/news.php
+++ weather/news.php
@@ -90,7 +90,7 @@
 									$sql = "SELECT one_week FROM week_forecast ORDER BY ind DESC LIMIT 1;";
 									$result = $conn->query($sql)->fetch_assoc();
 
-									print_r(json_decode($result["one_week"])["day_1"]);
+									print_r(json_decode($result["one_week"]));
 
 								?>
 							</div>
