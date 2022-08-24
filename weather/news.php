7f2529006446a9e4d9a3d57b5ff12dc060101f62
Zdravkoski <lazar@samplesolutions.eu>
2022-08-24 20:04:13 +0200

    news change

--- weather/news.php
+++ weather/news.php
@@ -61,14 +61,7 @@
 				</div>
 				
 
-				<div class="fullwidth-block">
-					<div class="container">
-						
-					</div>
-				</div>
-			</main> <!-- .main-content -->
-
-			<div class="forecast-table">
+				<div class="forecast-table">
 				<div class="container">
 					<div class="forecast-container">
 						<div class="today forecast">
@@ -133,6 +126,30 @@
 					</div>
 				</div>
 			</div>
+			</main> <!-- .main-content -->
+
+			<footer class="site-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8">
+							<form action="#" class="subscribe-form">
+								<input type="text" placeholder="Enter your email to subscribe...">
+								<input type="submit" value="Subscribe">
+							</form>
+						</div>
+						<div class="col-md-3 col-md-offset-1">
+							<div class="social-links">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-google-plus"></i></a>
+								<a href="#"><i class="fa fa-pinterest"></i></a>
+							</div>
+						</div>
+					</div>
+
+					<p class="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
+				</div>
+			</footer> <!-- .site-footer -->
 		</div>
 		
 		<script src="js/jquery-1.11.1.min.js"></script>
