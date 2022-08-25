6e8282971a6d322f5178d8a5d40413a926df1b1d
Zdravkoski <lazar@samplesolutions.eu>
2022-08-25 04:15:53 +0200

    dawwa

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,10 @@
 import smtplib, ssl
 import json
+import sys
+
+f_name, email_addr, email_body = sys.argw[1], sys.argw[2], sys.argw[3]
 
 def send_mail():
+	f_name, email_addr, email_body = f_name, email_addr, email_body
 	print("da")
 
