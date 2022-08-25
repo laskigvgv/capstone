754cf4a4ff862a86687cd64d90be712921ccc491
Zdravkoski <lazar@samplesolutions.eu>
2022-08-26 01:25:40 +0200

    new contact

--- send_mail.py
+++ send_mail.py
@@ -2,6 +2,9 @@ import smtplib, ssl
 import json
 import sys
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 f_name , email_addr, email_body = sys.argv[1], sys.argv[2], sys.argv[3]
 print(email_body)
 def send_mail(f_name , email_addr, email_body):
@@ -10,10 +13,13 @@ def send_mail(f_name , email_addr, email_body):
 	port = 587
 	sender_email = "zdr.lazar.salesrocks@gmail.com"
 	passw = "audrdgvpsbkyrqjl"
-
 	email_to = "zdravkoski.lazar@gmail.com"
 
 	context = ssl.create_default_context()
+	message = MIMEMultipart("alternative")
+	message["Subject"] = "Contact Form from {}".format(email_addr)
+	message["From"] = sender_email
+	message["To"] = email_to
 
 	try:
 	    server = smtplib.SMTP(smtp_server,port)
@@ -23,15 +29,14 @@ def send_mail(f_name , email_addr, email_body):
 	    server.login(sender_email, passw)
 	    # TODO: Send email here
 
-	    email_message = """
-	    Subject: TEST SUBJECT\n\n
+	    text = """Email from {}
 
-	    Email Message From {email_addr}
+	    {}""".format(email_addr, email_body)
 
-	    {email_body}
-	    """.format(email_addr = email_addr, email_body = email_body)
+	    part1 = MIMEText(text, "plain")
 
-	    server.sendmail(sender_email, email_to, email_message)
+	    message.attach(part1)
+	    server.sendmail(sender_email, email_to, message)
 
 	except Exception as e:
 	    # Print any error messages to stdout
