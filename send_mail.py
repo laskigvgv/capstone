e81869e032d16190029acb0515c2dde8a52e7976
Zdravkoski <lazar@samplesolutions.eu>
2022-08-26 00:15:52 +0200

    new contact

--- send_mail.py
+++ send_mail.py
@@ -5,6 +5,37 @@ import sys
 f_name , email_addr, email_body= sys.argv[1], sys.argv[2], sys.argv[3]
 
 def send_mail(f_name , email_addr, email_body):
-	print(f_name, "	", email_addr,"	", email_body)
+	smtp_server = "smtp.gmail.com"
+	port = 587
+	sender_email = "zdr.lazar.salesrocks@gmail.com"
+	passw = "audrdgvpsbkyrqjl"
+
+	email_to = "zdravkoski.lazar@gmail.com"
+
+	context = ssl.create_default_context()
+
+	try:
+	    server = smtplib.SMTP(smtp_server,port)
+	    server.ehlo()
+	    server.starttls(context=context)
+	    server.ehlo()
+	    server.login(sender_email, password)
+	    # TODO: Send email here
+
+	    email_message = """
+	    Email Message From {email_addr}
+
+	    {email_body}""".format(email_addr = email_addr, email_body = email_body)
+
+	    server.sendmail(sender_email, receiver_email, message)
+
+	except Exception as e:
+	    # Print any error messages to stdout
+	    print(e)
+	finally:
+	    server.quit() 
+
+	print("ISPRATEN EMAIL ||| KRAJ NA SEND_MAIL")
+
 
 send_mail(f_name , email_addr, email_body)
