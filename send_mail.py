92fb68a176f5838cf2ae1bb4f842d147eb57db7d
Zdravkoski <lazar@samplesolutions.eu>
2022-08-25 21:25:56 +0200

    lala

--- send_mail.py
+++ send_mail.py
@@ -2,6 +2,6 @@ import smtplib, ssl
 import json
 import sys
 
-f_name, email_addr, email_body = sys.argw[1], sys.argw[2], sys.argw[3]
+# f_name, email_addr, email_body = sys.argw[1]
 
-print(f_name, "	",email_addr,"	",email_body)
+print("DA DA DA")
