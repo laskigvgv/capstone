35e55c6074d3cd17bc5b9b893a3723542e37f59f
Zdravkoski <lazar@samplesolutions.eu>
2022-08-25 21:00:23 +0200

    lala

--- send_mail.py
+++ send_mail.py
@@ -4,8 +4,4 @@ import sys
 
 f_name, email_addr, email_body = sys.argw[1], sys.argw[2], sys.argw[3]
 
-def send_mail():
-	f_name, email_addr, email_body = f_name, email_addr, email_body
-	print("da")
-
-print(send_mail)
+print("DA DA DA ")
