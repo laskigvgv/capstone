49849f7ea970ce69a2aa0b01c1f5bd71e1c027d1
Zdravkoski <lazar@samplesolutions.eu>
2022-08-26 01:12:34 +0200

    new contact

--- send_mail.py
+++ send_mail.py
@@ -4,43 +4,43 @@ import sys
 
 f_name , email_addr= sys.argv[1], sys.argv[2]
 email_body = "{}".format(sys.argv[3])
-print(sys.argv[3])
-# def send_mail(f_name , email_addr, email_body):
-# 	print("EMAIL BODY NA POCETOK NA FUNKCIJA	", email_body)
-# 	smtp_server = "smtp.gmail.com"
-# 	port = 587
-# 	sender_email = "zdr.lazar.salesrocks@gmail.com"
-# 	passw = "audrdgvpsbkyrqjl"
 
-# 	email_to = "zdravkoski.lazar@gmail.com"
+def send_mail(f_name , email_addr, email_body):
 
-# 	context = ssl.create_default_context()
+	smtp_server = "smtp.gmail.com"
+	port = 587
+	sender_email = "zdr.lazar.salesrocks@gmail.com"
+	passw = "audrdgvpsbkyrqjl"
 
-# 	try:
-# 	    server = smtplib.SMTP(smtp_server,port)
-# 	    server.ehlo()
-# 	    server.starttls(context=context)
-# 	    server.ehlo()
-# 	    server.login(sender_email, passw)
-# 	    # TODO: Send email here
+	email_to = "zdravkoski.lazar@gmail.com"
 
-# 	    email_message = """
-# 	    Subject: Contact Form
+	context = ssl.create_default_context()
 
-# 	    Email Message From {email_addr}
+	try:
+	    server = smtplib.SMTP(smtp_server,port)
+	    server.ehlo()
+	    server.starttls(context=context)
+	    server.ehlo()
+	    server.login(sender_email, passw)
+	    # TODO: Send email here
 
-# 	    {email_body}
-# 	    """.format(email_addr = email_addr, email_body = email_body)
+	    email_message = """
+	    Subject: Contact Form
 
-# 	    server.sendmail(sender_email, email_to, email_message)
+	    Email Message From {email_addr}
 
-# 	except Exception as e:
-# 	    # Print any error messages to stdout
-# 	    print(e)
-# 	finally:
-# 	    server.quit() 
+	    {email_body}
+	    """.format(email_addr = email_addr, email_body = email_body)
 
-# 	# print("ISPRATEN EMAIL ||| KRAJ NA SEND_MAIL 	", email_body)
+	    server.sendmail(sender_email, email_to, email_message)
+
+	except Exception as e:
+	    # Print any error messages to stdout
+	    print(e)
+	finally:
+	    server.quit() 
+
+	print("ISPRATEN EMAIL ||| KRAJ NA SEND_MAIL 	", email_body)
 
 
 # send_mail(f_name , email_addr, email_body)
