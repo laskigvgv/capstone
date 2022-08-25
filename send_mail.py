44cfab81598b888208070a8af87f405cadfb7630
Zdravkoski <lazar@samplesolutions.eu>
2022-08-25 04:08:40 +0200

    dawwa

--- send_mail.py
+++ send_mail.py
@@ -2,8 +2,5 @@ import smtplib, ssl
 import json
 
 def send_mail(f_name, email_addr, email_body):
-	json.dumps("da":"odlicno")
+	return "da"
 
-
-if __name__ == "__mail__":
-	print(send_mail(f_name, email_addr, email_body))
