3bd16d8728f6d426c9e1da24758e6c8f942c6c60
Zdravkoski <lazar@samplesolutions.eu>
2022-08-26 02:33:58 +0200

    new contact

--- send_mail.py
+++ send_mail.py
@@ -2,44 +2,55 @@ import smtplib, ssl
 import json
 import sys
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 f_name , email_addr, email_body = sys.argv[1], sys.argv[2], sys.argv[3]
 print(email_body)
 def send_mail(f_name , email_addr, email_body):
-	print("EMAIL BODY NA POCETOK NA FUNKCIJA	", email_body)
-	smtp_server = "smtp.gmail.com"
-	port = 587
 	sender_email = "zdr.lazar.salesrocks@gmail.com"
-	passw = "audrdgvpsbkyrqjl"
+	receiver_email = "zdravkoski.lazar@gmail.com"
+	password = "audrdgvpsbkyrqjl"
 
-	email_to = "zdravkoski.lazar@gmail.com"
+	message = MIMEMultipart("alternative")
+	message["Subject"] = "multipart test"
+	message["From"] = sender_email
+	message["To"] = receiver_email
 
+	# Create the plain-text and HTML version of your message
+	text = """\
+	Hi,
+	How are you?
+	Real Python has many great tutorials:
+	www.realpython.com"""
+	html = """\
+	<html>
+	  <body>
+	    <p>Hi,<br>
+	       How are you?<br>
+	       <a href="http://www.realpython.com">Real Python</a> 
+	       has many great tutorials.
+	    </p>
+	  </body>
+	</html>
+	"""
+
+	# Turn these into plain/html MIMEText objects
+	part1 = MIMEText(text, "plain")
+	part2 = MIMEText(html, "html")
+
+	# Add HTML/plain-text parts to MIMEMultipart message
+	# The email client will try to render the last part first
+	message.attach(part1)
+	message.attach(part2)
+
+	# Create secure connection with server and send email
 	context = ssl.create_default_context()
+	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+	    server.login(sender_email, password)
+	    server.sendmail(
+	    sender_email, receiver_email, message.as_string()
+	    )
 	
-	try:
-	    server = smtplib.SMTP(smtp_server,port)
-	    server.ehlo()
-	    server.starttls(context=context)
-	    server.ehlo()
-	    server.login(sender_email, passw)
-	    # TODO: Send email here
-
-	    email_message = """
-	    Subject: TEST SUBJECT\n\n
-
-	    Email Message From {email_addr}
-
-	    {email_body}
-	    """.format(email_addr = email_addr, email_body = email_body)
-
-	    server.sendmail(sender_email, email_to, email_message)
-
-	except Exception as e:
-	    # Print any error messages to stdout
-	    print(e)
-	finally:
-	    server.quit() 
-
-	# print("ISPRATEN EMAIL ||| KRAJ NA SEND_MAIL 	", email_body)
-
 
 send_mail(f_name , email_addr, email_body)
