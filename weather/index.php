9d4a1f8bd29f222dbe499221cac74bec7d410f47
Zdravkoski <lazar@samplesolutions.eu>
2022-08-27 01:17:11 +0200

    no echo

--- weather/index.php
+++ weather/index.php
@@ -191,15 +191,77 @@ Approximately 26 km (16 mi) from Gostivar is a ski resort, "Zare Lazarevski", in
 					</div>
 				</div>
 
-				
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-4">
+								<h2 class="section-title">Application features</h2>
+								<ul class="arrow-feature">
+									<li>
+										<h3>Natus error sit voluptatem accusantium</h3>
+										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
+									</li>
+									<li>
+										<h3>Natus error sit voluptatem accusantium</h3>
+										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
+									</li>
+									<li>
+										<h3>Natus error sit voluptatem accusantium</h3>
+										<p>Doloremque laudantium totam rem aperiam Inventore veritatis et quasi architecto beatae vitae.</p>
+									</li>
+								</ul>
+							</div>
+							<div class="col-md-4">
+								<h2 class="section-title">Weather analyssis</h2>
+								<ul class="arrow-list">
+									<li><a href="#">Accusantium doloremque laudantium rem aperiam</a></li>
+									<li><a href="#">Eaque ipsa quae ab illo inventore veritatis quasi</a></li>
+									<li><a href="#">Architecto beatae vitae dicta sunt explicabo</a></li>
+									<li><a href="#">Nemo enim ipsam voluptatem quia voluptas</a></li>
+									<li><a href="#">Aspernatur aut odit aut fugit, sed quia consequuntur</a></li>
+									<li><a href="#">Magni dolores eos qui ratione voluptatem sequi</a></li>
+									<li><a href="#">Neque porro quisquam est qui dolorem ipsum quia</a></li>
+								</ul>
+							</div>
+							<div class="col-md-4">
+								<h2 class="section-title">Awesome Photos</h2>
+								<div class="photo-grid">
+									<a href="#"><img src="images/thumb-1.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-2.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-3.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-4.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-5.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-6.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-7.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-8.jpg" alt="#"></a>
+									<a href="#"><img src="images/thumb-9.jpg" alt="#"></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main> <!-- .main-content -->
 
 			<footer class="site-footer">
 				<div class="container">
 					<div class="row">
-						<p class="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
+						<div class="col-md-8">
+							<form action="#" class="subscribe-form">
+								<input type="text" placeholder="Enter your email to subscribe...">
+								<input type="submit" value="Subscribe">
+							</form>
+						</div>
+						<div class="col-md-3 col-md-offset-1">
+							<div class="social-links">
+								<a href="#"><i class="fa fa-facebook"></i></a>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+								<a href="#"><i class="fa fa-google-plus"></i></a>
+								<a href="#"><i class="fa fa-pinterest"></i></a>
+							</div>
+						</div>
 					</div>
 
-					
+					<p class="colophon">Copyright 2014 Company name. Designed by Themezy. All rights reserved</p>
 				</div>
 			</footer> <!-- .site-footer -->
 		</div>
