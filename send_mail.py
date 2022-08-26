70400c3a1575df9f2c17e6dccebde32b3b9b4295
Zdravkoski <lazar@samplesolutions.eu>
2022-08-26 02:39:51 +0200

    new contact

--- send_mail.py
+++ send_mail.py
@@ -17,16 +17,13 @@ def send_mail(f_name , email_addr, email_body):
 	message["From"] = sender_email
 	message["To"] = receiver_email
 
-	# Create the plain-text and HTML version of your message
 	text = """\
 	{}""".format(email_body)
 
-	# Turn these into plain/html MIMEText objects
 	part1 = MIMEText(text, "plain")
 
 	message.attach(part1)
 
-	# Create secure connection with server and send email
 	context = ssl.create_default_context()
 	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
 	    server.login(sender_email, password)
