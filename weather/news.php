778202c1f8ce513b267a6a76e04b5bd59f5f37a1
Zdravkoski <lazar@samplesolutions.eu>
2022-08-27 02:53:07 +0200

    no echo

--- weather/news.php
+++ weather/news.php
@@ -40,7 +40,6 @@
 			$result = $conn->query($sql)->fetch_assoc();
 			$decoded_result = json_decode($result["one_week"],true);
 			$day_in_week = date("l", strtotime($decoded_result["day_1"]["date_from_unix"]));
-			echo date('l', strtotime($decoded_result["day_1"]["date_from_unix"]));
 
 ?>
 
